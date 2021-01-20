dffe2867996125bfec1b9337864af04a1c707bd2
lihort <65966569+lihort@users.noreply.github.com>
2021-01-20 12:50:26 -0500

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -168,13 +168,13 @@ function DarkLib:Window()
 
 	Title.Name = "Title"
 	Title.Parent = MainFrame
-	Title.BackgroundColor3 = Color3.fromRGB(255, 0,0)
+	Title.BackgroundColor3 = Color3.fromRGB(255,0,0)
 	Title.BackgroundTransparency = 1.000
 	Title.Position = UDim2.new(0.42778793, 0, 0.041837737, 0)
 	Title.Size = UDim2.new(0, 78, 0, 30)
 	Title.Font = Enum.Font.Gotham
 	Title.Text = "PP X"
-	Title.TextColor3 = Color3.fromRGB(168, 168, 168)
+	Title.TextColor3 = Color3.fromRGB(255, 0, 0)
 	Title.TextSize = 20.000
 
 	Containers.Name = "Containers"
